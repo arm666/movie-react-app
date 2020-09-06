4bc5663d9835cd5c4553ccce70ce635bb1158ac4
gorkhali <gorkhali9999@gmail.com>
2020-09-06 13:56:23 +0545

    Updates

--- precache-manifest.154fc4d534453daf32bc4b4035675776.js
+++ precache-manifest.154fc4d534453daf32bc4b4035675776.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b9c7012f994a5acc41829a9ca46b960",
+    "revision": "f22c31331ea74ae7375915c3a0fcff82",
     "url": "/movie-react-app/index.html"
   },
   {
-    "revision": "3484ae99fa54abe83da7",
+    "revision": "6477859f7a401b8e9780",
     "url": "/movie-react-app/static/css/main.38c02f80.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-react-app/static/js/2.6f4837d5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3484ae99fa54abe83da7",
-    "url": "/movie-react-app/static/js/main.7a904a5a.chunk.js"
+    "revision": "6477859f7a401b8e9780",
+    "url": "/movie-react-app/static/js/main.5e2326f1.chunk.js"
   },
   {
     "revision": "7f91aa8e79e77c4bac6c",
