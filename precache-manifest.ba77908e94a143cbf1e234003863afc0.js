a5cf0345cce26e69ababff5fd88c292018f92210
gorkhali <gorkhali9999@gmail.com>
2020-09-06 14:09:37 +0545

    Updates

--- precache-manifest.ba77908e94a143cbf1e234003863afc0.js
+++ precache-manifest.ba77908e94a143cbf1e234003863afc0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1ece43576d1db7a55a6c615b40c020a",
+    "revision": "3f678aef21691ffd259622110c1fcdf8",
     "url": "/movie-react-app/index.html"
   },
   {
-    "revision": "c27103c6dea4dbe038e8",
+    "revision": "2c7b7795d767f670726a",
     "url": "/movie-react-app/static/css/main.38c02f80.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie-react-app/static/js/2.6f4837d5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c27103c6dea4dbe038e8",
-    "url": "/movie-react-app/static/js/main.3f51c50c.chunk.js"
+    "revision": "2c7b7795d767f670726a",
+    "url": "/movie-react-app/static/js/main.9d80d375.chunk.js"
   },
   {
     "revision": "7f91aa8e79e77c4bac6c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-react-app/precache-manifest.ea2f0ae551f5a25ae16c58241688303a.js"
+  "/movie-react-app/precache-manifest.ba77908e94a143cbf1e234003863afc0.js"
 );
 
 self.addEventListener('message', (event) => {
